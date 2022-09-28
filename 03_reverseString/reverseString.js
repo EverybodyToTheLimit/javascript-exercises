ce1fcbc45a457897a8f4cf866efd112a92da8197
EverybodyToTheLimit <bartoszzawlocki@gmail.com>
2022-09-28 21:45:50 +0100

    Test 3 completed

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,13 @@
-const reverseString = function() {
+const reverseString = function(string) {
 
+let unoReverso = Array.from(string);
+let targetArray = [];
+let stopIterating = unoReverso.length;
+for (i = 0; i < stopIterating; i++) {
+    targetArray.push(unoReverso.pop());
+}
+let result = targetArray.join("");
+return(result);
 };
 
 // Do not edit below this line
