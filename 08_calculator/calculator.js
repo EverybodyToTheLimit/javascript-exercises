02cc56a4afe738eabfb0dc106d2a700e2cdb4335
EverybodyToTheLimit <bartoszzawlocki@gmail.com>
2022-09-30 17:47:51 +0100

    test 8 done

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,48 @@
-const add = function() {
-	
+const add = function(a,b) {
+	return(a+b)
 };
 
-const subtract = function() {
-	
+const subtract = function(a,b) {
+	return(a-b);
 };
 
-const sum = function() {
-	
+const sum = function(a) {
+  let result =0
+  a.map(countFunction);
+  function countFunction (num){
+    result = result + num;
+  }
+  return(result);
 };
 
-const multiply = function() {
+// const multiply = function(a) {
+//   let numberArguments = arguments.length;
+//   let result = multiply.arguments[0];
+//   for (i=1; i<numberArguments; i++) {
+//     result = result * multiply.arguments[i];
+//   }
+//   return(result);
+// };
 
+const multiply = function(a) {
+  let result = 1
+  a.map(multiplyFunction);
+  function multiplyFunction(num) {
+    result = result * num; 
+  }
+  return(result);
+}
+
+const power = function(a, b) {
+	return(Math.pow(a, b));
 };
 
-const power = function() {
-	
-};
-
-const factorial = function() {
-	
+const factorial = function(a) {
+	let result = 1;
+  for (let i=a; i>0; i--) {
+    result = result * i
+  }
+  return(result);
 };
 
 // Do not edit below this line
