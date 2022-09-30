28c5f24c7d4e68b9695bebc7d208c217d33d55cf
EverybodyToTheLimit <bartoszzawlocki@gmail.com>
2022-09-30 10:37:23 +0100

    Test 4 completed

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,10 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array) {
+    let sourceArray = arguments[0];
+    for (i=1; i < arguments.length; i++) {
+       let searchString = arguments[i];
+       sourceArray = sourceArray.filter(e => e !== searchString);
+    }
+    return(sourceArray);
 };
 
 // Do not edit below this line
