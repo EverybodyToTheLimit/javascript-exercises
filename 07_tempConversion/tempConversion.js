f19434e81c4386b054ba6ff963d1b5134522f5f3
EverybodyToTheLimit <bartoszzawlocki@gmail.com>
2022-09-30 16:57:19 +0100

    test 7 completed

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,8 +1,15 @@
-const ftoc = function() {
+function roundToTwo(num) {
+  return +(Math.round(num + "e+1")  + "e-1");
+}
+
+const ftoc = function(input) {
+
+  return(roundToTwo(Number(((input-32)/1.8))));
 
 };
 
-const ctof = function() {
+const ctof = function(input) {
+  return(roundToTwo(Number(((1.8*input)+32))));
 
 };
 
