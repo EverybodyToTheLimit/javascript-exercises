567664fa98786b640f127180f06cd7ca5a1275c1
EverybodyToTheLimit <bartoszzawlocki@gmail.com>
2022-09-30 18:25:28 +0100

    test 9 done

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,4 +1,21 @@
-const palindromes = function () {
+const palindromes = function (input) {
+let sanitisedString = input.split(" ");
+sanitisedString = sanitisedString.join("");
+sanitisedString = sanitisedString.replaceAll(/,/g,'');
+sanitisedString = sanitisedString.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g, "");
+sanitisedString = sanitisedString.toLowerCase();
+let originalArray = sanitisedString.split("")
+let reversedArray = originalArray.reverse();
+let reversedString = reversedArray.toString();
+reversedString = reversedString.split(",");
+reversedString = reversedString.join("");
+reversedString = reversedString.toLowerCase();
+if (sanitisedString === reversedString) {
+    return(true)
+}
+else {
+    return(false)
+}
 
 };
 
