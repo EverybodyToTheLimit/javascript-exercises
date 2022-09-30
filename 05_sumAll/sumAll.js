924b5f63458073d6388ba7da3e0b1536db7129c2
EverybodyToTheLimit <bartoszzawlocki@gmail.com>
2022-09-30 11:17:43 +0100

    test 5 completed

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,23 @@
-const sumAll = function() {
+const sumAll = function(number1, number2) {
+    if (Number.isInteger(number1) && number1 >= 0 && Number.isInteger(number2) && number2 >= 0) {
+        let result = null;
+        if (number1 < number2) {
+            result = number1;
+            for (i=number1; i<number2; i++) {
+                result = result + (i + 1);
+            }
+        }
+        else {
+            result = number2;
+            for (i=number2; i<number1; i++) {
+                result = result + (i + 1);
+        }
+        }
+        return(result);
+    }
+    else {
+        return("ERROR")
+    }
 
 };
 
